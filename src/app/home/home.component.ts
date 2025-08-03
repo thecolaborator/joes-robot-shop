55bce7840625d3b1ad67078379bab5c19067ac9d
Grant Christiansen <grant.christiansen@usanainc.com>
2025-08-02 22:44:10 -0600

    home page v1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,16 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
-  // template: '<p class="red">Inline home works!</p>',
-  template: `\
-    <p class="red">
-      Inline home works!
-    </p>`,
-  styles: [`\
-    .red {
-      color: red;
-    }
-  `]  
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
